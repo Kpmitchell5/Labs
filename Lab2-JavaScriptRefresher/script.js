60a8f152505c4cdfcab28bfed141096c831a349c
Kevin <kpmitchell5>
2022-10-12 22:07:45 -0400

    JS Refresher

---
+++ Lab2-JavaScriptRefresher/script.js
@@ -0,0 +1,15 @@
+const firstName = "Kevin";
+const lastName = "Mitchell";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let population = 301286;
+console.log(population);
+console.log(population * 3);
+const javaScriptIsCool = true;
+let colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
