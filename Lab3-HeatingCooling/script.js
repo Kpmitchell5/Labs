ca42a455a54cb6a69f464bc2b8f772b75dfd59d8
Kevin <kpmitchell5>
2022-10-17 13:23:54 -0400

    the lab

---
+++ Lab3-HeatingCooling/script.js
@@ -0,0 +1,26 @@
+let actualTemp = 50;
+let desiredTemp = 70;
+
+if (actualTemp < desiredTemp) {
+  console.log("Run heat");
+} else if (actualTemp > desiredTemp) {
+  console.log("Run A/C");
+} else {
+  console.log("Standby");
+}
+
+// Extended Challenge
+let targetUnit = "F";
+let tempCelsius = 32;
+
+switch (targetUnit) {
+  case "F":
+    console.log(((tempCelsius - 32) * 5) / 9);
+    break;
+  case "K":
+    console.log(tempCelsius - 273.15);
+    break;
+  case "C":
+    console.log(tempCelsius);
+    break;
+}
